3966fad8812ff84d8e44cab92c6978581acfc6b3
KrvyFT <KrvyFT@proton.me>
2024-07-24 18:14:48 +0800

    Implemented locks, but not fully functional

--- src/audio.rs
+++ src/audio.rs
@@ -1,48 +1,103 @@
-use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-use cpal::{Device, InputCallbackInfo, OutputCallbackInfo, Stream};
+use std::sync::{Arc, Mutex};
+
+use cpal::traits::{DeviceTrait, HostTrait};
+use cpal::{Device, Host, InputCallbackInfo, OutputCallbackInfo, Stream, StreamConfig};
+use ringbuf::storage::Heap;
 use ringbuf::traits::{Consumer, Producer, Split};
-use ringbuf::HeapRb;
+use ringbuf::wrap::caching::Caching;
+use ringbuf::{HeapRb, SharedRb};
 
 use crate::effects::filter::Filter;
 
+pub type producer = Caching<Arc<SharedRb<Heap<f32>>>, true, false>;
+pub type consumer = Caching<Arc<SharedRb<Heap<f32>>>, false, true>;
+
 pub struct Wave {
-    pub input_stream: Stream,
-    pub output_stream: Stream,
+    pub host: Host,
+    pub config: StreamConfig,
+    pub input_device: Device,
+    pub output_device: Device,
+    pub input_stream: Option<Stream>,
+    pub output_stream: Option<Stream>,
     pub filter: Filter,
 }
 
-impl Wave {
-    pub fn create_stream() -> (Stream, Stream) {
-        let host =
-        cpal::host_from_id(cpal::available_hosts()
-        .into_iter()
-        .find(|id| *id == cpal::HostId::Jack)
-        .expect(
-        "make sure --features jack is specified. only works on OSes where jack is available",
-        )).expect("jack host unavailable");
+pub struct SharedWave {
+    pub wave: Arc<Mutex<Wave>>,
+}
 
-        let (config, input_device, output_device) = Self::device_config(host);
-
+impl SharedWave {
+    pub fn create_stream(
+        config: &StreamConfig,
+        input_device: &Device,
+        output_device: &Device,
+    ) -> (Stream, Stream) {
         let ring = HeapRb::<f32>::new(config.sample_rate.0 as usize * config.channels as usize);
         let (producer, consumer) = ring.split();
 
         let input_stream = input_device
-            .build_input_stream(&config, Self::handle_input(producer), Self::err_fn, None)
+            .build_input_stream(
+                &config,
+                Self::default_handle_input(producer),
+                Self::err_fn,
+                None,
+            )
             .unwrap();
         let output_stream = output_device
-            .build_output_stream(&config, Self::handle_output(consumer), Self::err_fn, None)
+            .build_output_stream(
+                &config,
+                Self::default_handle_output(consumer),
+                Self::err_fn,
+                None,
+            )
             .unwrap();
         (input_stream, output_stream)
     }
 
-    fn device_config(host: cpal::Host) -> (cpal::StreamConfig, Device, Device) {
+    pub fn change_stream(
+        self,
+        handle_input: impl FnMut(&[f32], &InputCallbackInfo) + Send + 'static,
+        handle_output: impl FnMut(&mut [f32], &OutputCallbackInfo) + Send + 'static,
+    ) -> (Stream, Stream) {
+        let guard = self.wave.lock().unwrap();
+        let input_stream = guard
+            .input_device
+            .build_input_stream(&guard.config, handle_input, Self::err_fn, None)
+            .unwrap();
+        let output_stream = guard
+            .output_device
+            .build_output_stream(&guard.config, handle_output, Self::err_fn, None)
+            .unwrap();
+        (input_stream, output_stream)
+    }
+
+    pub fn create_buffer(&self) -> (producer, consumer) {
+        let guard = self.wave.lock().unwrap();
+
+        let ring = HeapRb::<f32>::new(
+            guard.config.sample_rate.0 as usize * guard.config.channels as usize,
+        );
+        let (producer, consumer) = ring.split();
+        (producer, consumer)
+    }
+
+    pub fn device_config(host: &cpal::Host) -> (cpal::StreamConfig, Device, Device) {
         let input_device = host.default_input_device().expect("没有找到输入设备");
         let output_device = host.default_output_device().expect("没有找到输出设备");
         let config: cpal::StreamConfig = input_device.default_input_config().unwrap().into();
         (config, input_device, output_device)
     }
 
-    fn handle_input(
+    pub fn host() -> Host {
+        cpal::host_from_id(cpal::available_hosts()
+            .into_iter()
+            .find(|id| *id == cpal::HostId::Jack)
+            .expect(
+            "make sure --features jack is specified. only works on OSes where jack is available",
+            )).expect("jack host unavailable")
+    }
+
+    pub fn default_handle_input(
         mut producer: ringbuf::wrap::caching::Caching<
             std::sync::Arc<ringbuf::SharedRb<ringbuf::storage::Heap<f32>>>,
             true,
@@ -56,7 +111,7 @@ impl Wave {
         }
     }
 
-    fn handle_output(
+    pub fn default_handle_output(
         mut consumer: ringbuf::wrap::caching::Caching<
             std::sync::Arc<ringbuf::SharedRb<ringbuf::storage::Heap<f32>>>,
             false,
@@ -70,7 +125,80 @@ impl Wave {
         }
     }
 
-    fn err_fn(err: cpal::StreamError) {
+    pub fn err_fn(err: cpal::StreamError) {
+        eprintln!("an error occurred on stream: {}", err);
+    }
+}
+
+impl Wave {
+    pub fn change_stream(
+        self,
+        handle_input: impl FnMut(&[f32], &InputCallbackInfo) + Send + 'static,
+        handle_output: impl FnMut(&mut [f32], &OutputCallbackInfo) + Send + 'static,
+    ) -> (Stream, Stream) {
+        let input_stream = self
+            .input_device
+            .build_input_stream(&self.config, handle_input, Self::err_fn, None)
+            .unwrap();
+        let output_stream = self
+            .output_device
+            .build_output_stream(&self.config, handle_output, Self::err_fn, None)
+            .unwrap();
+        (input_stream, output_stream)
+    }
+
+    pub fn create_buffer(&self) -> (producer, consumer) {
+        let ring =
+            HeapRb::<f32>::new(self.config.sample_rate.0 as usize * self.config.channels as usize);
+        let (producer, consumer) = ring.split();
+        (producer, consumer)
+    }
+
+    pub fn device_config(host: &cpal::Host) -> (cpal::StreamConfig, Device, Device) {
+        let input_device = host.default_input_device().expect("没有找到输入设备");
+        let output_device = host.default_output_device().expect("没有找到输出设备");
+        let config: cpal::StreamConfig = input_device.default_input_config().unwrap().into();
+        (config, input_device, output_device)
+    }
+
+    pub fn host() -> Host {
+        cpal::host_from_id(cpal::available_hosts()
+        .into_iter()
+        .find(|id| *id == cpal::HostId::Jack)
+        .expect(
+        "make sure --features jack is specified. only works on OSes where jack is available",
+        )).expect("jack host unavailable")
+    }
+
+    pub fn default_handle_input(
+        mut producer: ringbuf::wrap::caching::Caching<
+            std::sync::Arc<ringbuf::SharedRb<ringbuf::storage::Heap<f32>>>,
+            true,
+            false,
+        >,
+    ) -> impl FnMut(&[f32], &InputCallbackInfo) + Send + 'static {
+        move |data: &[f32], _info: &InputCallbackInfo| {
+            for &sample in data {
+                producer.try_push(sample).unwrap();
+            }
+        }
+    }
+
+    pub fn default_handle_output(
+        mut consumer: ringbuf::wrap::caching::Caching<
+            std::sync::Arc<ringbuf::SharedRb<ringbuf::storage::Heap<f32>>>,
+            false,
+            true,
+        >,
+    ) -> impl FnMut(&mut [f32], &OutputCallbackInfo) + Send + 'static {
+        move |data: &mut [f32], _info: &OutputCallbackInfo| {
+            for sample in data {
+                *sample = consumer.try_pop().unwrap_or(0.0);
+            }
+        }
+    }
+
+    pub fn err_fn(err: cpal::StreamError) {
         eprintln!("an error occurred on stream: {}", err);
     }
 }
--- src/ui.rs
+++ src/ui.rs
@@ -1,8 +1,12 @@
-use cpal::traits::StreamTrait;
-use iced::widget::{column, container, slider, text};
-use iced::{Center, Element, Fill};
+use std::sync::{Arc, Mutex};
 
-use crate::audio::Wave;
+use cpal::traits::StreamTrait;
+use cpal::{InputCallbackInfo, OutputCallbackInfo};
+use iced::widget::{button, column, container, slider, text};
+use iced::{Center, Element, Fill};
+use ringbuf::traits::{Consumer, Producer};
+
+use crate::audio::{SharedWave, Wave};
 use crate::effects::filter::Filter;
 
 #[derive(Debug, Clone, Copy)]
@@ -11,24 +15,65 @@ pub enum Message {
     FilterSliderChanged(i32),
 }
 
-impl Wave {
+impl SharedWave {
     pub fn new() -> Self {
-        let (input_stream, output_stream) = Self::create_stream();
+        let host = Self::host();
+        let (config, input_device, output_device) = Self::device_config(&host);
+        let (input_stream, output_stream) =
+            Self::create_stream(&config, &input_device, &output_device);
         input_stream.play().unwrap();
         output_stream.play().unwrap();
         Self {
-            input_stream,
-            output_stream,
-            filter: Filter::default(),
+            wave: Arc::new(Mutex::new(Wave {
+                host,
+                config,
+                input_device,
+                output_device,
+                input_stream: Some(input_stream),
+                output_stream: Some(output_stream),
+                filter: Filter {
+                    used: false,
+                    alpha: 0.0,
+                },
+            })),
         }
     }
 
     pub fn update(&mut self, message: Message) {
         match message {
             Message::EnableFilter => {
-                self.filter.used = true;
+                let mut guard = *self.wave.lock().unwrap();
+                guard.filter.used = true;
+                drop(guard.input_stream.take());
+                drop(guard.output_stream.take());
+
+                let (mut producer, mut consumer) = self.create_buffer();
+                let filter = guard.filter;
+
+                let (input_stream, output_stream) = guard.change_stream(
+                    move |data: &[f32], _info: &InputCallbackInfo| {
+                        for &sample in data {
+                            producer.try_push(sample).unwrap();
+                        }
+                    },
+                    move |data: &mut [f32], _info: &OutputCallbackInfo| {
+                        for sample in data {
+                            *sample = filter.low_pass_filter(consumer.try_pop().unwrap_or(0.0));
+                            println!("{}", filter.alpha);
+                        }
+                    },
+                );
+
+                input_stream.play().unwrap();
+                output_stream.play().unwrap();
+
+                guard.input_stream = Some(input_stream);
+                guard.output_stream = Some(output_stream);
             }
-            Message::FilterSliderChanged(alpha) => self.filter.alpha = alpha as f32,
+            Message::FilterSliderChanged(alpha) => {
+                let guard = self.wave.lock().unwrap();
+                guard.filter.alpha = alpha as f32
+            }
         }
     }
 
@@ -45,8 +90,8 @@ impl Wave {
         .width(250);
 
         let text = text(self.filter.alpha / 1000.0);
-
-        column![slider, text,]
+        let button = button("Filter").on_press(Message::EnableFilter);
+        column![button, slider, text,]
             .width(Fill)
             .align_x(Center)
             .spacing(20)
