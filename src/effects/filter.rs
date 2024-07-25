a110027fe73541db22b6abef79cb30a2675a4a91
KrvyFT <KrvyFT@proton.me>
2024-07-25 21:23:17 +0800

    Add dedicated handler for filter processing

--- src/effects/filter.rs
+++ src/effects/filter.rs
@@ -1,3 +1,7 @@
+use cpal::OutputCallbackInfo;
+
+use crate::audio::{AUDIO_CHANNELS, EFF_CHANNELS};
+
 #[derive(Debug, Clone, Copy)]
 pub struct Filter {
     pub used: bool,
@@ -18,6 +22,29 @@ impl Filter {
         unsafe {
             LAST_SAMPLE = alpha * sample + (1.0 - alpha) * LAST_SAMPLE;
             LAST_SAMPLE
+        }
+    }
+
+    pub fn filter_handle_output() -> impl FnMut(&mut [f32], &OutputCallbackInfo) + Send + 'static {
+        move |data: &mut [f32], _info: &OutputCallbackInfo| {
+            static mut FILTER: Filter = Filter {
+                used: true,
+                alpha: 0.0,
+            };
+
+            let mut alpha = unsafe { FILTER.alpha };
+            if let Ok(v) = EFF_CHANNELS.1.try_recv() {
+                println!("{}", v);
+                unsafe {
+                    FILTER.alpha = v;
+                }
+                alpha = v;
+            } else {
+            };
+
+            for sample in data {
+                *sample = Filter::low_pass_filter(alpha, AUDIO_CHANNELS.1.recv().unwrap_or(0.0))
+            }
         }
     }
 }
